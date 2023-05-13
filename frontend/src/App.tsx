21d245d4ed55c01698de2a8486ca3a0677294033
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2023-05-13 20:54:44 +0200

    add video upload component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,8 +2,9 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import './App.css'
 import {Header} from './component/header/Header'
 import {Video} from './component/video/Video'
-import { Login } from './component/auth/Login'
-import { Register } from './component/auth/Register'
+import {Login} from './component/auth/Login'
+import {Register} from './component/auth/Register'
+import {UploadVideo} from './component/upload/UploadVideo'
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
             <Route path="/" element={<Video />} />
             <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
+            <Route path="/video-upload" element={<UploadVideo />} />
           </Routes>
         </main>
       </BrowserRouter>
---
+++ frontend/src/component/upload/UploadVideo.tsx
@@ -0,0 +1,94 @@
+import React, {useState, useEffect} from 'react'
+import './UploadVideo.css'
+import axios, {AxiosProgressEvent, AxiosRequestConfig} from 'axios'
+
+const UploadVideo = () => {
+  const [progress, setProgress] = useState(0)
+  const [selectedFile, setSelectedFile] = useState<File | null>(null)
+  const [presignedUrl, setPresignedUrl] = useState<string | null>(null)
+  const [error, setError] = useState<string | null>(null)
+
+  const handleFileInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files?.length) {
+      const file = e.target.files[0]
+
+      if (file.type.startsWith('video/')) {
+        setSelectedFile(file)
+        setError(null)
+      } else {
+        setSelectedFile(null)
+        setError('Please upload a video file.')
+      }
+    }
+  }
+
+  const getPresignedUrl = async (fileName: string) => {
+    try {
+      const response = await axios.get(
+        `http://localhost:1337/generate-presigned-url?fileName=${fileName}`
+      )
+      setPresignedUrl(response.data.url)
+    } catch (error) {
+      console.error('Error while getting presigned URL', error)
+      setError('Failed to upload the file. Please try again.')
+    }
+  }
+
+  useEffect(() => {
+    if (selectedFile) {
+      getPresignedUrl(selectedFile.name)
+    }
+  }, [selectedFile])
+
+  const uploadFile = () => {
+    if (selectedFile && presignedUrl) {
+      const config: AxiosRequestConfig = {
+        headers: {
+          'Content-Type': selectedFile.type,
+        },
+        onUploadProgress: function (progressEvent: AxiosProgressEvent) {
+          if (progressEvent.total) {
+            const percentCompleted = Math.round(
+              (progressEvent.loaded * 100) / progressEvent.total
+            )
+            setProgress(percentCompleted)
+          }
+        },
+      }
+
+      axios
+        .put(presignedUrl, selectedFile, config)
+        .then((res) => console.log(res))
+        .catch((err) => console.error(err))
+    }
+  }
+
+  return (
+    <div>
+      <h1>Upload a Video</h1>
+      <p>Choose a video file and upload it.</p>
+      <label htmlFor="fileUpload">Video file:</label>
+      <input
+        id="fileUpload"
+        type="file"
+        accept="video/*"
+        onChange={handleFileInput}
+        aria-describedby="fileUploadError"
+      />
+      {error && (
+        <div id="fileUploadError" role="alert" style={{color: 'red'}}>
+          {error}
+        </div>
+      )}
+      <label htmlFor="uploadProgress">Upload Progress:</label>
+      <progress id="uploadProgress" value={progress} max="100">
+        {progress}%
+      </progress>
+      <button onClick={uploadFile} disabled={!selectedFile || !!error}>
+        Upload Video
+      </button>
+    </div>
+  )
+}
+
+export {UploadVideo}

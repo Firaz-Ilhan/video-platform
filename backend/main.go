b96befcf9bb08655f511b0ea37950c7b25f122fd
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2023-05-29 18:39:47 +0200

    add /random-file endpoint

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"time"
@@ -21,29 +22,33 @@ type Config struct {
 	AWSAccessKeyID     string
 	AWSSecretAccessKey string
 	S3Bucket           string
+	S3OutputBucket     string
 	Port               string
 }
 
-func loadConfig() (*Config, error) {
+var (
+	config Config
+	r      *rand.Rand
+	s3Svc  *s3.S3
+)
+
+func init() {
 	err := godotenv.Load()
 	if err != nil {
-		return nil, fmt.Errorf("error loading .env file: %w", err)
+		log.Fatalf("Failed to load .env file: %v", err)
 	}
 
-	return &Config{
+	config = Config{
 		S3Region:           os.Getenv("S3_REGION"),
 		AWSAccessKeyID:     os.Getenv("AWS_ACCESS_KEY_ID"),
 		AWSSecretAccessKey: os.Getenv("AWS_SECRET_ACCESS_KEY"),
 		S3Bucket:           os.Getenv("S3_BUCKET"),
+		S3OutputBucket:     os.Getenv("S3_OUTPUT_BUCKET"),
 		Port:               os.Getenv("PORT"),
-	}, nil
-}
-
-func main() {
-	config, err := loadConfig()
-	if err != nil {
-		log.Fatal(err)
 	}
+
+	src := rand.NewSource(time.Now().UnixNano())
+	r = rand.New(src)
 
 	sess, err := session.NewSession(&aws.Config{
 		Region: aws.String(config.S3Region),
@@ -57,34 +62,14 @@ func main() {
 		log.Fatalf("Failed to create AWS session: %v", err)
 	}
 
-	s3Svc := s3.New(sess)
+	s3Svc = s3.New(sess)
+}
 
+func main() {
 	router := gin.Default()
 
-	router.GET("/generate-presigned-url", func(c *gin.Context) {
-		fileName := c.Query("fileName")
-		if fileName == "" {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "fileName parameter is required"})
-			return
-		}
-
-		req, _ := s3Svc.PutObjectRequest(&s3.PutObjectInput{
-			Bucket: aws.String(config.S3Bucket),
-			Key:    aws.String(fileName),
-		})
-
-		str, err := req.Presign(15 * time.Minute)
-
-		if err != nil {
-			log.Printf("Failed to sign request: %v", err)
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to sign request"})
-			return
-		}
-
-		c.JSON(http.StatusOK, gin.H{
-			"url": str,
-		})
-	})
+	router.GET("/generate-presigned-url", generatePresignedURL)
+	router.GET("/random-file", randomFile)
 
 	c := cors.New(cors.Options{
 		AllowedOrigins:   []string{"http://localhost:3000"},
@@ -98,4 +83,68 @@ func main() {
 	port := fmt.Sprintf(":%s", config.Port)
 	log.Printf("Starting server on %s", port)
 	log.Fatal(http.ListenAndServe(port, handler))
+}
+
+func generatePresignedURL(c *gin.Context) {
+	fileName := c.Query("fileName")
+	if fileName == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "fileName parameter is required"})
+		return
+	}
+
+	req, _ := s3Svc.PutObjectRequest(&s3.PutObjectInput{
+		Bucket: aws.String(config.S3Bucket),
+		Key:    aws.String(fileName),
+	})
+
+	str, err := req.Presign(15 * time.Minute)
+
+	if err != nil {
+		log.Printf("Failed to sign request: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to sign request"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"url": str,
+	})
+}
+
+func randomFile(c *gin.Context) {
+	input := &s3.ListObjectsV2Input{
+		Bucket: aws.String(config.S3OutputBucket),
+	}
+
+	result, err := s3Svc.ListObjectsV2(input)
+	if err != nil {
+		log.Printf("Failed to list objects: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to list objects"})
+		return
+	}
+
+	if len(result.Contents) == 0 {
+		c.JSON(http.StatusNotFound, gin.H{"error": "No files found in the bucket."})
+		return
+	}
+
+	randomIndex := r.Intn(len(result.Contents))
+	randomFile := result.Contents[randomIndex]
+
+	req, _ := s3Svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(config.S3OutputBucket),
+		Key:    randomFile.Key,
+	})
+
+	presignedURL, err := req.Presign(15 * time.Minute)
+
+	if err != nil {
+		log.Printf("Failed to sign request: %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to sign request"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"url":      presignedURL,
+		"fileName": *randomFile.Key,
+	})
 }

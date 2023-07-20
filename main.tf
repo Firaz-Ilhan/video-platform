6253525df8f3108b8f842e9ea0e8878ef5df9a6f
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2023-07-20 18:50:53 +0200

    add more variables to tf

--- main.tf
+++ main.tf
@@ -1,9 +1,14 @@
+variable "aws_region" {
+  default = "eu-central-1"
+}
+
 variable "access_key" {}
 variable "secret_key" {}
-
+variable "first_bucket_name" {}
+variable "second_bucket_name" {}
 
 provider "aws" {
-  region     = "eu-central-1"
+  region     = var.aws_region
   access_key = var.access_key
   secret_key = var.secret_key
 }

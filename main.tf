a15a2fb093ec930c8290c74abfde2b34f5974dae
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2023-07-23 19:50:28 +0200

    add trigger for the lambda function

--- main.tf
+++ main.tf
@@ -73,3 +73,20 @@ resource "aws_iam_role_policy_attachment" "lambda_exec_policy_attach" {
   role       = aws_iam_role.lambda_exec.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.first_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.media_conversion_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.media_conversion_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.first_bucket.arn
+}

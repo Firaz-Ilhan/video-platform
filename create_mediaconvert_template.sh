dd3b2c0d3ecb31cccb32e815ff193f21759db2c0
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2023-07-26 21:11:52 +0200

    add comments to the mediaconvert script

--- create_mediaconvert_template.sh
+++ create_mediaconvert_template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# enter the S3 bucket name
+# enter the S3 bucket name like "s3://my-bucket-name/"
 s3_bucket="${S3_BUCKET}"
 
 payload='
@@ -75,13 +75,17 @@ payload='
   "HopDestinations": []
 }'
 
+# replace the placeholder with the S3 bucket name
 payload=$(echo "${payload}" | sed "s|s3_bucket_placeholder|${S3_BUCKET}|g")
 
+# create the template file
 echo "${payload}" >template.json
 
 # get the endpoint for the MediaConvert service of the specific region
 endpoint_url=$(aws mediaconvert describe-endpoints --query 'Endpoints[0].Url' --output text)
 
+# create the template
 aws mediaconvert create-job-template --endpoint-url ${endpoint_url} --cli-input-json file://template.json
 
+# delete the template file
 rm template.json

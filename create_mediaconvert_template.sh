b8676ed1bda5adff58b32308655f8d8b729dc535
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2023-07-26 19:50:37 +0200

    add a script to create a mediaconvert template

---
+++ create_mediaconvert_template.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+
+# enter the S3 bucket name
+s3_bucket="s3://name/"
+
+payload='
+{
+  "Name": "my_template",
+  "Settings": {
+    "TimecodeConfig": {
+      "Source": "ZEROBASED"
+    },
+    "OutputGroups": [
+      {
+        "Name": "File Group",
+        "Outputs": [
+          {
+            "ContainerSettings": {
+              "Container": "MP4",
+              "Mp4Settings": {}
+            },
+            "VideoDescription": {
+              "CodecSettings": {
+                "Codec": "H_264",
+                "H264Settings": {
+                  "ParNumerator": 16,
+                  "MaxBitrate": 250000,
+                  "ParDenominator": 9,
+                  "RateControlMode": "QVBR",
+                  "SceneChangeDetect": "TRANSITION_DETECTION",
+                  "ParControl": "SPECIFIED"
+                }
+              }
+            },
+            "AudioDescriptions": [
+              {
+                "AudioSourceName": "Audio Selector 1",
+                "CodecSettings": {
+                  "Codec": "AAC",
+                  "AacSettings": {
+                    "Bitrate": 96000,
+                    "CodingMode": "CODING_MODE_2_0",
+                    "SampleRate": 48000
+                  }
+                }
+              }
+            ]
+          }
+        ],
+        "OutputGroupSettings": {
+          "Type": "FILE_GROUP_SETTINGS",
+          "FileGroupSettings": {
+            "Destination": "s3_bucket_placeholder"
+          }
+        }
+      }
+    ],
+    "Inputs": [
+      {
+        "AudioSelectors": {
+          "Audio Selector 1": {
+            "DefaultSelection": "DEFAULT"
+          }
+        },
+        "VideoSelector": {},
+        "TimecodeSource": "ZEROBASED"
+      }
+    ]
+  },
+  "AccelerationSettings": {
+    "Mode": "DISABLED"
+  },
+  "StatusUpdateInterval": "SECONDS_60",
+  "Priority": 0,
+  "HopDestinations": []
+}'
+
+payload=$(echo "${payload}" | sed "s|s3_bucket_placeholder|${s3_bucket}|g")
+
+echo "${payload}" >template.json
+
+# get the endpoint for the MediaConvert service of the specific region
+endpoint_url=$(aws mediaconvert describe-endpoints --query 'Endpoints[0].Url' --output text)
+
+aws mediaconvert create-job-template --endpoint-url ${endpoint_url} --cli-input-json file://template.json
+
+rm template.json

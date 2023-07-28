86983315330b4c809ad731ab2ce75a23728c0cef
Firaz Ilhan <39162520+Firaz-Ilhan@users.noreply.github.com>
2023-07-28 21:05:54 +0200

    fetch videos

--- frontend/src/hooks/useAxios.tsx
+++ frontend/src/hooks/useAxios.tsx
@@ -2,11 +2,12 @@ import {useState, useEffect} from 'react'
 import axios, {AxiosError, AxiosRequestConfig, AxiosResponse} from 'axios'
 
 const useAxios = (axiosParams: AxiosRequestConfig) => {
-  const [response, setResponse] = useState<AxiosResponse>()
-  const [error, setError] = useState<AxiosError>()
+  const [response, setResponse] = useState<AxiosResponse | undefined>()
+  const [error, setError] = useState<AxiosError | undefined>()
   const [loading, setLoading] = useState(true)
 
   const fetchData = async (params: AxiosRequestConfig) => {
+    setLoading(true)
     try {
       const result = await axios.request(params)
       setResponse(result)
